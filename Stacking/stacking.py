c43af4d25879bfff524124a6a9718e9c3d631203
raselpy <rasel.py@gmail.com>
2025-01-06 12:14:29 +0100

    Implement GridSearchCV for StackingClassifier with RandomForest and GradientBoosting
    - Added a StackingClassifier with RandomForestClassifier and GradientBoostingClassifier as base estimators.
    - Implemented hyperparameter tuning using GridSearchCV to optimize:
      - n_estimators for both RandomForest and GradientBoosting.
      - Final estimator as LogisticRegression with different regularization strengths.
      - Passthrough option for including original features in the final estimator.
    - Added print_results function to display GridSearchCV results, including best parameters and performance scores.

--- Stacking/stacking.py
+++ Stacking/stacking.py
@@ -1,8 +1,34 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.ensemble import RandomForestClassifier,StackingClassifier,GradientBoostingClassifier
+from sklearn.ensemble import StackingClassifier, GradientBoostingClassifier, RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
-import matplotlib.pyplot as plt
+
+tr_features = pd.read_csv('../train_features.csv')
+tr_labels = pd.read_csv('../train_labels.csv')
+
+def print_results(results):
+    print('BEST PARAMS: {}\n'.format(results.best_params_))
+
+    means = results.cv_results_['mean_test_score']
+    stds = results.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, results.cv_results_['params']):
+        print('{} (+/-{}) for {}'.format(round(mean, 3), round(std * 2, 3), params))
+
+estimators = [('rf', RandomForestClassifier()),
+              ('gb', GradientBoostingClassifier())]
+
+sc = StackingClassifier(estimators=estimators)
+sc.get_params()
+parameters = {
+    'gb__n_estimators': [50, 100],
+    'rf__n_estimators': [50, 100],
+    'final_estimator': [LogisticRegression(C=0.1),
+                        LogisticRegression(C=1),
+                        LogisticRegression(C=10)],
+    'passthrough': [True, False]
+}
+cv = GridSearchCV(sc, parameters, cv=5)
+cv.fit(tr_features, tr_labels.values.ravel())
+
+print_results(cv)
+print(cv.best_estimator_)
